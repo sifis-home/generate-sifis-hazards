3ac2fa83e24988180379d259e3c6278e7ed149c1
Luni-4 <luni-4@hotmail.it>
2023-05-04 17:44:04 +0200

    Generate hazards with an external template

--- src/toolchain/mod.rs
+++ src/toolchain/mod.rs
@@ -1,8 +1,9 @@
+pub mod external;
 pub mod rust;
 
+pub(crate) use external::*;
 pub(crate) use rust::*;
 
-#[macro_export]
 macro_rules! builtin_templates {
     ($root:expr => $(($name:expr, $template:expr)),+) => {
         [
@@ -15,3 +16,16 @@ macro_rules! builtin_templates {
         ]
     }
 }
+
+fn build_source(templates: &[(&str, &str)]) -> minijinja::Source {
+    let mut source = minijinja::Source::new();
+    for (name, src) in templates {
+        source
+            .add_template(*name, *src)
+            .expect("Internal error, built-in template");
+    }
+
+    source
+}
+
+pub(crate) use builtin_templates;
